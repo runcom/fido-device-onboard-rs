a66a5ea4c63ed7d251ff22bd549f95fdc4ae52c4
Antonio Murdaca <runcom@linux.com>
2021-11-09 09:34:52 +0100

    feat(store): implement metadata storage
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- store/src/in_memory.rs
+++ store/src/in_memory.rs
@@ -7,23 +7,26 @@ use async_trait::async_trait;
 use log::trace;
 use tokio::sync::RwLock;
 
+use crate::MetadataValue;
+
 use super::Store;
 use super::StoreError;
 
-type ValueT<V> = (Option<SystemTime>, V);
+type ValueT<V> = (Option<SystemTime>, V, Arc<RwLock<HashMap<String, Vec<u8>>>>);
 
 #[derive(Debug)]
 struct MemoryStore<K, V> {
     store: Arc<RwLock<HashMap<K, ValueT<V>>>>,
 }
 
-pub(super) fn initialize<OT, K, V>(
+pub(super) fn initialize<OT, K, V, MKT>(
     _cfg: Option<config::Value>,
-) -> Result<Box<dyn Store<OT, K, V>>, StoreError>
+) -> Result<Box<dyn Store<OT, K, V, MKT>>, StoreError>
 where
     OT: crate::StoreOpenMode,
     K: std::string::ToString + Eq + std::hash::Hash + Send + Sync + 'static,
     V: Send + Sync + Clone + 'static,
+    MKT: crate::MetadataLocalKey + 'static,
 {
     Ok(Box::new(MemoryStore {
         store: Arc::new(RwLock::new(HashMap::new())),
@@ -31,11 +34,12 @@ where
 }
 
 #[async_trait]
-impl<OT, K, V> Store<OT, K, V> for MemoryStore<K, V>
+impl<OT, K, V, MKT> Store<OT, K, V, MKT> for MemoryStore<K, V>
 where
     OT: crate::StoreOpenMode,
     K: std::string::ToString + Eq + std::hash::Hash + Send + Sync,
     V: Send + Sync + Clone,
+    MKT: crate::MetadataLocalKey,
 {
     async fn load_data(&self, key: &K) -> Result<Option<V>, StoreError> {
         trace!("Looking for entry {}", key.to_string());
@@ -46,7 +50,7 @@ where
             trace!("Entry not found");
             return Ok(None);
         }
-        let (ttl, data) = data.unwrap();
+        let (ttl, data, _metadata) = data.unwrap();
         if ttl.is_some() && ttl.unwrap() < SystemTime::now() {
             trace!("Entry had expired");
             return Ok(None);
@@ -55,12 +59,38 @@ where
         return Ok(Some(data.clone()));
     }
 
+    async fn store_metadata(
+        &self,
+        key: &K,
+        metadata_key: &crate::MetadataKey<MKT>,
+        metadata_value: &dyn MetadataValue,
+    ) -> Result<(), StoreError> {
+        trace!("Looking for entry {}", key.to_string());
+        let store = self.store.read().await;
+        let data = store.get(key);
+
+        if data.is_none() {
+            trace!("Entry not found");
+            return Ok(());
+        }
+
+        let (_ttl, _data, metadata_store) = data.unwrap();
+        metadata_store.write().await.insert(
+            metadata_key.to_key().to_string(),
+            metadata_value.to_stored()?,
+        );
+        Ok(())
+    }
+
     async fn store_data(&self, key: K, ttl: Option<Duration>, value: V) -> Result<(), StoreError> {
         trace!("Storing entry, key {}, TTL {:?}", key.to_string(), ttl);
 
         let ttl = ttl.map(|d| SystemTime::now() + d);
 
-        self.store.write().await.insert(key, (ttl, value));
+        self.store
+            .write()
+            .await
+            .insert(key, (ttl, value, Arc::new(RwLock::new(HashMap::new()))));
         Ok(())
     }
 
